fcf66ccfe39dd9c07fe4a7d609a2e52d7eeee41a
William Bellamy <willbell@c02qq3usg8wp.itvplc.ads>
2016-05-03 15:01:06 +0100

    Fib, curry and uncurry

--- src/main/scala/FuncProgrammingChapter2.scala
+++ src/main/scala/FuncProgrammingChapter2.scala
@@ -1,9 +1,80 @@
 package FuncProgrammingChapter2
-/**
-  * Created by willbell on 4/29/16.
-  */
+
+import scala.annotation.tailrec
+
 object FuncProgrammingChapter2 {
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): BigInt = {
+    @annotation.tailrec
+    def fib_tail( n: Int, a:Int, acc:Int): Int = n match {
+      case 0 => a
+      case _ => fib_tail( n-1, acc, a + acc )
+    }
+    fib_tail(n, 0, 1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+
+    @annotation.tailrec
+    def sorted_tail(n: Int, as: Array[A]): Boolean =
+      if (n >= (as.length -1)) true
+      else if ( ordered(as(n), as(n + 1)) ) sorted_tail(n + 1, as)
+      else false
+
+    sorted_tail(0, as)
+  }
+
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+    b => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b:B) => f(a,b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
 
 }
+
+object MyModule {
+
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  def factorial(n: Int): Int = {
+
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+
+      go(n, 1)
+  }
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d."
+    msg.format(x, abs(x))
+  }
+
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    println(formatAbs(-42))
+
+    println(formatFactorial(7))
+
+  }
+
+}
+
+
--- src/test/scala/TestFib.scala
+++ src/test/scala/TestFib.scala
@@ -2,6 +2,28 @@ import org.scalatest.FunSuite
 
 import FuncProgrammingChapter2.FuncProgrammingChapter2
 
-class QuickStartSpec extends FunSuite {
-  FuncProgrammingChapter2.fib(2) 
+class TestFib extends FunSuite {
+
+  test("Fib 1 to 5") {
+
+    assert( FuncProgrammingChapter2.fib(0) === 0)
+    assert( FuncProgrammingChapter2.fib(1) === 1)
+    assert( FuncProgrammingChapter2.fib(2) === 1)
+    assert( FuncProgrammingChapter2.fib(3) === 2)
+    assert( FuncProgrammingChapter2.fib(4) === 3)
+    assert( FuncProgrammingChapter2.fib(5) === 5)
+    assert( FuncProgrammingChapter2.fib(6) === 8)
+    assert( FuncProgrammingChapter2.fib(7) === 13)
+  }
+
+  test("Is sorted for array of ints") {
+
+
+    val intCompare: (Int, Int) => Boolean = (x, y) => x <= y
+
+    assert( FuncProgrammingChapter2.isSorted[Int](Array(1, 2, 3, 4), intCompare) )
+    assert( FuncProgrammingChapter2.isSorted[Int](Array(1, 1, 2, 3, 4), intCompare) )
+    assert( ! FuncProgrammingChapter2.isSorted[Int](Array(1, 3, 2, 4), (x, y) => x <= y) )
+
+  }
 }
