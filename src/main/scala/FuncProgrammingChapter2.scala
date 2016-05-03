c34373e0884e39d982019995c4419eb8a239ea04
William Bellamy <willbell@c02qq3usg8wp.itvplc.ads>
2016-05-03 09:19:03 +0100

    initial fib skeleton

---
+++ src/main/scala/FuncProgrammingChapter2.scala
@@ -0,0 +1,9 @@
+package FuncProgrammingChapter2
+/**
+  * Created by willbell on 4/29/16.
+  */
+object FuncProgrammingChapter2 {
+
+  def fib(n: Int): Int = ???
+
+}
---
+++ src/test/scala/TestFib.scala
@@ -0,0 +1,7 @@
+import org.scalatest.FunSuite
+
+import FuncProgrammingChapter2.FuncProgrammingChapter2
+
+class QuickStartSpec extends FunSuite {
+  FuncProgrammingChapter2.fib(2) 
+}
